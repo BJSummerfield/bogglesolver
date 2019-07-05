fbf21a1a5e8876d216fd0080e856cf118f2ee392
Brian Summerfield <brianjsummerfield@gmail.com>
2019-07-05 00:22:14 -0500

    working on trie

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,8 +1,18 @@
 import Board from './models/board.js'
+import Trie from './models/trie.js'
 import Ui from './views/ui.js'
+import Dictionary from './models/bogglewords.js'
 
 var cells = document.querySelector('.dice-container')
 const board = new Board(4,4)
 const ui = new Ui({ element: cells, board})
+const trie = new Trie
+console.log(trie)
 ui.boardSetup()
 console.log(board)
+
+// setTimeOut(function() {
+// 	trie.containsWord('retarded')
+// }, 5000)
+
+setTimeout(function(){ trie.containsWord('happying'); }, 3000);
---
+++ public/javascript/models/trie.js
@@ -0,0 +1,64 @@
+import Dictionary from './bogglewords.js'
+class Trie {
+	constructor() {
+		this.trie = {}
+		this.wordList = []
+		this.addWords()
+	}
+
+	addWords() {
+		var dictionary = new Dictionary
+		var length = dictionary.words.length
+		var word = dictionary.words
+		for (var i = 0; i < length; i++) {
+			var currentNode = this.trie
+			word[i].toLowerCase().split('').forEach((letter, index) => {
+				if(!currentNode[letter]) {
+					currentNode[letter] = {}
+					currentNode[letter].isWord = false
+					
+				}
+				if(index === word[i].length - 1) {
+					currentNode.isWord = true
+				} else {
+					currentNode = currentNode[letter]
+
+				}
+			})
+		// this.containsWord(word[i])
+		}
+	}
+
+  containsWord(word) {
+    var currentNode = this.trie
+    var split = word.split('')
+    split.forEach(function(letter) {
+    	if(!currentNode[letter]){
+    		return false
+    	}
+    	currentNode = currentNode[letter]
+    })
+    console.log(currentNode)
+    console.log(word, currentNode.isWord)
+    return currentNode.isWord
+  }
+
+  isPrefix(prefix) {
+    var currentNode = this.trie
+    return prefix.split('').every(letter => {
+      if(!currentNode[letter]) {
+        return false;
+      }
+      currentNode = currentNode[letter]
+      if (currentNode.isWord === true) {
+      	if (!this.wordList.includes(prefix)) {
+      		this.wordList.push(prefix)
+      	}
+    	}
+      return true
+    });
+  }
+
+}
+
+export default Trie
